dd58482214889bc150afafcd271849684dce8d01
igorrochap <igor.systemof@gmail.com>
2022-12-14 22:54:57 -0300

    feat: filtering noisy temperature measurements

--- projeto2.ino
+++ projeto2.ino
@@ -7,17 +7,23 @@
 */
 
 #include <LiquidCrystal_I2C.h>
- 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 
 const int pwm = 9;
 const int minSensor = -550;
 const int maxSensor = 1500;
+const int variationCutoff = 100;
+const int runningAverageCount = 50;
+int nextRunningAverage;
 
 int sensor = 0;
 float voltage = 0;
+float lastVoltage;
+bool firstIteration = true;
 int rpm = 0;
 double temp = 0;
+float runningAverageBuffer[runningAverageCount];
+int filteredTemp = 0;
 
 void setup()
 {
@@ -30,26 +36,51 @@ void setup()
 void loop()
 {
   getTemperature();
+  filterTemperature();
   controlRPM();
 
   lcd.clear();
   printOnLCD();
 
-  delay(800);
+  firstIteration = false;
+
+  delay(300);
 }
 
 void getTemperature() {
   sensor = analogRead(A0);
   voltage = sensor * (5000 / 1024.0);
+  if(!firstIteration) {
+    if(abs(voltage - lastVoltage) >= variationCutoff || voltage == 0) {
+      voltage = lastVoltage;
+    }
+  }
+  lastVoltage = voltage;
   temp = voltage / 10;
+}
+
+void filterTemperature() {
+  runningAverageBuffer[nextRunningAverage++] = temp;
+
+  if (nextRunningAverage >= runningAverageCount)
+  {
+    nextRunningAverage = 0; 
+  }
+
+  for(int i=0; i < runningAverageCount; ++i)
+  {
+    filteredTemp += runningAverageBuffer[i];
+  }
+
+  filteredTemp /= runningAverageCount;
 }
 
 void controlRPM() {
   rpm = 0;
-  if(temp >= 20) {
+  if(filteredTemp >= 20) {
     rpm = map(voltage, minSensor, maxSensor, 0, 255);
   } 
-  if (temp >= 60) {
+  if (filteredTemp >= 60) {
     rpm = 255;
   }
   analogWrite(pwm, rpm);
@@ -57,7 +88,7 @@ void controlRPM() {
 
 void printOnLCD() {
   int realRPM = map(rpm, 0, 255, 0, 8505);
-  lcd.print("TEMP: " + String(temp) + " C");
+  lcd.print("TEMP: " + String(filteredTemp) + " C");
   lcd.setCursor(0, 1);
   lcd.print("RPM: " + String(realRPM));
 }
